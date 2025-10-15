e7d4e352fa4a54287333f3d7c5308b23ea81540a
meekrob <davidcking.inbox@gmail.com>
2025-10-15 15:43:40 -0600

    fastp: add json output and convert to array job

--- 01_Fastp/runDeDupFastpHTML.sh
+++ 01_Fastp/runDeDupFastpHTML.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-
 #SBATCH --partition=day-long-cpu
 #SBATCH --job-name=fastpDeDupLoop
-#SBATCH --output=%x.%j.out
-#SBATCH --time=6:00:00
+#SBATCH --output=%x.%A_%a.log 
+#SBATCH --time=2:00:00
 #SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haogg@colostate.edu
+# ls -1 RawFastq/*R1_001.fastq.gz | wc -l => 18
+# use 
+#   sbatch --array=0-17 runDeDupFastpHTML.sh 
+source $HOME/templates/common.rc
+conda_activate ebel
 
-module purge
-source activate base
-conda activate rnaPseudo
+echo "JOB ARRAY TASK: ${SLURM_ARRAY_TASK_ID:=1}, SLURM_NTASKS=${SLURM_NTASKS:=0}"
 
-for FILE in RawFastq/*R1_001.fastq.gz
-do
-  TWO=${FILE/_R1_/_R2_}
-  OUT=${TWO/#RawFastq\/}
-  TRIM1=${FILE/RawFastq\//DeDupTrimmed\/DeDupTrimmed.}
-  TRIM2=${TWO/RawFastq\//DeDupTrimmed\/DeDupTrimmed.}  
-  echo ${OUT}
-  fastp -i ${FILE} -I ${TWO} -o ${TRIM1} -O ${TRIM2} -h DeDupFastqHTMLs/${OUT}.html -w 12 --dedup
-  rm fastp.json
-done
+cmd_args=( $(ls RawFastq/*R1_001.fastq.gz) )
+FILE=${cmd_args[$SLURM_ARRAY_TASK_ID]}
+TWO=${FILE/_R1_/_R2_}
+OUT=${TWO/#RawFastq\/}
+TRIM1=${FILE/RawFastq\//DeDupTrimmed\/DeDupTrimmed.}
+TRIM2=${TWO/RawFastq\//DeDupTrimmed\/DeDupTrimmed.}  
+echo TRIM1=${TRIM1}
+echo TRIM2=${TRIM2}
+
+OUT=$(basename $OUT .fastq.gz)
+echo OUT=${OUT}
+cmd="fastp -i ${FILE} -I ${TWO} -o ${TRIM1} -O ${TRIM2} -j DeDupFastqJSONs/${OUT}.json -h DeDupFastqHTMLs/${OUT}.html -w $SLURM_NTASKS --dedup"
+echo "$cmd"
+time eval "$cmd"

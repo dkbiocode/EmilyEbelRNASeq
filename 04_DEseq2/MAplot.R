abffca50b31c430cd80e1bce08a21915518f2526
meekrob <davidcking.inbox@gmail.com>
2025-10-20 11:35:57 -0600

    better legends and aesthetics

--- 04_DEseq2/MAplot.R
+++ 04_DEseq2/MAplot.R
@@ -11,15 +11,20 @@ MAplot <- function(res, qthreshold=.1) {
   # filter for any kind of NAs that make the value undefined in the plotting range, or the color aes.
   res_df %<>% filter(is.finite(log(baseMean+1)) & is.finite(log2FoldChange))
   res_df %<>% mutate(sig = is.finite(padj) & (padj < qthreshold))
-  
-  res_df %>% ggplot(
+  res_df %<>% arrange(sig) # put the significant points ontop
+
+  maplot <- res_df %>% ggplot(
       aes(
         x=log10(baseMean+1),
         y=log2FoldChange,
         color=sig,
+        size=sig
       )) + geom_point() +
       scale_color_manual(values=c("grey", "blue")) +
-      geom_hline(yintercept=0) -> maplot
+      geom_hline(yintercept=0) +
+    labs(color=sprintf("q-value < %.2f", qthreshold)) +
+    scale_size_manual(values=c(.5,1), guide="none") 
+
   
   maplot
   return(maplot)

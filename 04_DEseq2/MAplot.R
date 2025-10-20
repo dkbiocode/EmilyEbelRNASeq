845400f06402a61c448ceddf6da09e462d33d5ba
meekrob <davidcking.inbox@gmail.com>
2025-10-20 11:19:21 -0600

    functional MAplot

--- 04_DEseq2/MAplot.R
+++ 04_DEseq2/MAplot.R
@@ -2,8 +2,20 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(magrittr)
-MAplot <- function(res) {
-  res_df <- as.data.frame(res) %>%
-    rownames_to_column("gene_id")
-
+MAplot <- function(res, qthreshold=.1) {
+  res_df <- as.data.frame(res)
+  if (! "gene_id" %in% colnames(res_df)) {
+    res_df %<>% rownames_to_column("gene_id")
+  }
+  res_df %<>% filter(is.finite(log(baseMean+1)) & is.finite(log2FoldChange))
+  res_df %<>% mutate(sig = is.finite(padj) & (padj < qthreshold))
+  
+  res_df %>% ggplot(
+      aes(
+        x=log10(baseMean+1),
+        y=log2FoldChange,
+        color=sig,
+      )) + geom_point() +
+      scale_color_manual(values=c("grey", "blue")) +
+      geom_hline(yintercept=0)
 }

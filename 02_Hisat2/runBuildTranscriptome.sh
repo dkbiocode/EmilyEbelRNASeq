ad4ec03756c2af5a9f27bc48279ecab2cc206339
meekrob <davidcking.inbox@gmail.com>
2025-10-27 11:50:42 -0600

    modifying script for my setup

--- 02_Hisat2/runBuildTranscriptome.sh
+++ 02_Hisat2/runBuildTranscriptome.sh
@@ -1,17 +1,11 @@
-#!/usr/bin//env bash
-
+#!/usr/bin/env bash
 #SBATCH --partition=day-long-cpu
-#SBATCH --job-name=hisatBuildAedesTrans
+#SBATCH --job-name=hisatBuildCxT
 #SBATCH --output=%x.%j.out
 #SBATCH --time=1:30:00
 #SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haogg@colostate.edu
-
-module purge
-source activate base
-conda activate rnaPseudo
-
-hisat2-build -p 24 --exon 68Genes.exon --ss 68Genes.ss AedesGenome68.fa AedesTranscriptome68
+source $HOME/templates/common.rc
+conda_activate ebel
+hisat2-build -p $SLURM_NTASKS --exon 68Genes.exon --ss 68Genes.ss AedesGenome68.fa AedesTranscriptome68

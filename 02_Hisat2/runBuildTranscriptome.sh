57f7bab6f896cce17f18fb36c8fabac2f20bbead
meekrob <davidcking.inbox@gmail.com>
2025-10-28 10:07:31 -0600

    Changes worked to build splice-aware index

--- 02_Hisat2/runBuildTranscriptome.sh
+++ 02_Hisat2/runBuildTranscriptome.sh
@@ -7,5 +7,16 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
 source $HOME/templates/common.rc
+echo "Using ${SLURM_NTASKS:=0} SLURM_NTASKS"
 conda_activate ebel
-hisat2-build -p $SLURM_NTASKS --exon 68Genes.exon --ss 68Genes.ss AedesGenome68.fa AedesTranscriptome68
+
+# prepared input files
+FA=HisatBaseFiles/Culex-tarsalis_knwr_CONTIGS_CtarK1.fa
+SS=HisatBaseFiles/CtarK1.ss 
+EXON=HisatBaseFiles/CtarK1.exon 
+
+# command
+cmd="hisat2-build -p $SLURM_NTASKS --exon $EXON --ss $SS $FA HisatIndices/CxT"
+echo $cmd
+time eval $cmd
+

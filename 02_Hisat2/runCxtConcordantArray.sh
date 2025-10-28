051b65b32859390f993cbc46955c9e46f2f01485
meekrob <davidcking.inbox@gmail.com>
2025-10-28 10:29:32 -0600

    more adaptation to my environment

--- 02_Hisat2/runCxtConcordantArray.sh
+++ 02_Hisat2/runCxtConcordantArray.sh
@@ -8,18 +8,15 @@
 #SBATCH --mail-user=$USER
 #SBATCH --mail-type=all
 #SBATCH --output=%x.%A-%a.log # gives slurm.ID.log
+source $HOME/templates/common.rc
+echo "Using SLURM_NTASKS=${SLURM_NTASKS:=0} SLURM_NTASKS"
+echo "Running SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID:=0}"
+conda_activate ebel
 
-echo "[$0] $SLURM_JOB_NAME $@" # log the command line
-
-module purge
-source activate base
-conda activate rnaPseudo
-
-date # timestamp
-
-filename=CulexTrimmed.txt # $1
+filename=FastqInputFileList.txt
 
 prefix="../01_Fastp/DeDupTrimmed/DeDupTrimmed."
+index_prefix="HisatIndices/Cxt"
 linenum=0
 while read -r line
 do
@@ -28,8 +25,10 @@ do
       TWO=${line/_R1_/_R2_}
       OUT=${TWO#$prefix}
       #Remove Prefix
-      echo ${OUT}
-      hisat2 --phred33 --rna-strandness RF -p 100 -x HisatIndices/culexT_tran -1 ${line} -2 ${TWO} --no-discordant -S CxtConcordant/$OUT.sam
+      echo "$linenum: OUT=${OUT}"
+      cmd="hisat2 --rna-strandness RF -p $SLURM_NTASKS -x $index_prefix -1 ${line} -2 ${TWO} --no-discordant -S CxtConcordant/$OUT.sam"
+      echo $cmd
+      # time eval $cmd
     fi
     linenum=$((linenum + 1))
 done < $filename
